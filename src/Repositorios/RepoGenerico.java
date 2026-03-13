71fcf7eb2a7e494251dcc13b56d57234776f0f71
Ciro Schot <cirschot@gmail.com>
2026-03-13 10:40:34 -0300

    Cambio el tipo de dato de la interfaz a int

--- src/Repositorios/RepoGenerico.java
+++ src/Repositorios/RepoGenerico.java
@@ -1,14 +1,16 @@
 package Repositorios;
 
+import Modelo.Media;
+
 import java.util.List;
 
 public interface RepoGenerico<T>
 {
     void agregar(T elemento);
 
-    void eliminar(String id);
+    void eliminar(int id);
 
-    T buscarPorId(String id);
+    T buscarPorId(int id);
 
     List<T> listar();
 }
