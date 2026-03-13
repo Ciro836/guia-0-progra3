2b1f0f2861bf1551aec81ce9762a662b9ebae9d5
Ciro Schot <cirschot@gmail.com>
2026-03-13 10:06:12 -0300

    Cree una clase abstracta para que expandan las otras clases. Utilizo Herencia

---
+++ src/Clases/Media.java
@@ -0,0 +1,57 @@
+package Clases;
+
+public abstract class Media
+{
+    private int id;
+    private String titulo;
+    private String creador;
+    private String genero;
+
+    public Media(int id, String titulo, String creador, String genero)
+    {
+        this.id = id;
+        this.titulo = titulo;
+        this.creador = creador;
+        this.genero = genero;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getTitulo()
+    {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo)
+    {
+        this.titulo = titulo;
+    }
+
+    public String getCreador()
+    {
+        return creador;
+    }
+
+    public void setCreador(String creador)
+    {
+        this.creador = creador;
+    }
+
+    public String getGenero()
+    {
+        return genero;
+    }
+
+    public void setGenero(String genero)
+    {
+        this.genero = genero;
+    }
+}
